e565719c1980c6f207a382a90002ee3758cf321d
Setjhaba <116145007+Setjhaba@users.noreply.github.com>
2023-09-14 13:02:40 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 const pool = require("./db");
 
 //middleware
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5000", "https://PERN-To-Do-App.onrender.com"]
+}));
 app.use(express.json());
 
 //routes//
